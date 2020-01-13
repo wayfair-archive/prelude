e30659a0044bd3c923a17ac0ae482e567d2669ad
Peter Tomaselli <ptomaselli@wayfair.com>
2020-01-13 15:56:35 -0500

    Swift 5.1 support * try a different way of organizing the sample workspace

---
+++ Example/DummyFramework/DummyFramework/DummyFramework.h
@@ -0,0 +1,19 @@
+//
+//  DummyFramework.h
+//  DummyFramework
+//
+//  Created by Peter Tomaselli on 1/13/20.
+//  Copyright © 2020 Wayfair. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DummyFramework.
+FOUNDATION_EXPORT double DummyFrameworkVersionNumber;
+
+//! Project version string for DummyFramework.
+FOUNDATION_EXPORT const unsigned char DummyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DummyFramework/PublicHeader.h>
+
+
